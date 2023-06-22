4ffa2b4e09ef76d6c6184ee5a8ee2d8cf9e1ca15
odoh-tc <tochukwuodoh03@gmail.com>
2023-06-22 02:33:41 -0700

    monty

---
+++ tank.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+/**
+ * f_pint - top print
+ * @head: head
+ * @x: argument
+ * Return: return nothing
+*/
+void f_pint(stack_t **head, unsigned int x)
+{
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", x);
+		fclose(y.file);
+		free(y.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*head)->n);
+}
